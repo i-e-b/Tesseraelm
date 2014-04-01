1e877944c73e28c6b2c782c1f2359736989f82ab
i-e-b <ieb@me.com>
2014-04-01 22:16:57 +0100

    playing with paths and titles

--- Main.elm
+++ Main.elm
@@ -1,17 +1,28 @@
 
-
+port title : Signal String
+port title = always "Tesseraelm" <~ every second
 
 main = layers
     [ tiledImage 640 480 "/imgs/SandStone.jpg"
     , width 640 . centered <| style titleStyle (toText "Tesserae")
+    , shapes
     ]
 
 titleStyle : Style
 titleStyle = 
     { typeface = [ "Times New Roman", "serif" ]
-    , height   = Just 18
-    , color    = white
+    , height   = Just 72
+    , color    = black
     , bold     = True
     , italic   = False
     , line     = Nothing
     }
+
+--outline = path [ (50,50), (50,-50), (-50,-50), (-50,50), (50,50) ]
+outline = path [ (50,50), (25,-50), (-25,-50), (-50,50), (50,50) ]
+
+shapes = collage 200 420
+         [ outline |> traced (dashed black)  |> move (0, -110)
+         , outline |> traced (dotted black)
+         , outline |> traced (solid  black)   |> move (0, 110)
+         ]
