c67f9779e8be7814acb13908faf4d002b452d885
i-e-b <ieb@me.com>
2014-04-03 20:28:14 +0100

    Animation sketch

--- Main.elm
+++ Main.elm
@@ -22,7 +22,20 @@ titleStyle =
 outline = path [ (50,50), (25,-50), (-25,-50), (-50,50), (50,50) ]
 
 shapes = collage 200 420
-         [ outline |> traced (dashed black)  |> move (0, -110)
+         [ outline |> traced (dashed black) |> move (0, -110)
          , outline |> traced (dotted black)
-         , outline |> traced (solid  black)   |> move (0, 110)
+         , outline |> filled black          |> move (0, 110)
          ]
+
+{- -- Flipping animation --
+main = lift shapes (every millisecond)
+
+flipSpeed = 1 / 2500
+
+outline t = 
+  let x = abs (cos (t * flipSpeed)) * 50
+      y = cos (t * flipSpeed) * 100
+  in path [ (50,50), (x, 50 - y), (-x, 50 - y), (-50,50), (50,50) ]
+
+shapes t = collage 300 300 [outline t |> filled black]
+-}
