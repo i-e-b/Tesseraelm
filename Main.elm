a1344e815f476926af5919ee7d88f02160ffa825
i-e-b <ieb@me.com>
2014-04-09 21:09:39 +0100

    notes

--- Main.elm
+++ Main.elm
@@ -2,6 +2,27 @@
 port title : Signal String
 port title = always "Tesseraelm" <~ every second
 
+{--
+ - The plan:
+ -
+ - Build a board filled with random tiles (could build backwards later)
+ - Handle interaction in phases of idle, selected, animate
+ -}
+
+type Point ={x:Int, y:Int}
+type Start = Point
+type Dest = Point
+type Dests = [Point]
+type Prop = Float
+
+data Phase = Idle | Selected Start Dests | Animate Start Dest Prop
+
+type Game = {board:Board, phase:Phase}
+
+-- How to do the board? I don't want just and array of arrays...
+-- Maybe a quad-tree?
+
+{-- -- Text and drawing styles, background --
 main = layers
     [ tiledImage 640 480 "/imgs/SandStone.jpg"
     , width 640 . centered <| style titleStyle (toText "Tesserae")
@@ -26,16 +47,16 @@ shapes = collage 200 420
          , outline |> traced (dotted black)
          , outline |> filled black          |> move (0, 110)
          ]
+-}
+{--}-- Flipping animation --
+main = lift shapes (foldp (+) 0 (fps 15))
 
-{- -- Flipping animation --
-main = lift shapes (every millisecond)
-
-flipSpeed = 1 / 2500
+flipSpeed = 1 / 250
 
 outline t = 
-  let x = abs (cos (t * flipSpeed)) * 50
+  let x = abs (sin (t * flipSpeed)) * 20
       y = cos (t * flipSpeed) * 100
-  in path [ (50,50), (x, 50 - y), (-x, 50 - y), (-50,50), (50,50) ]
+  in path [ (50,50), (50 + x, 50 - y), (-50 - x, 50 - y), (-50,50), (50,50) ]
 
 shapes t = collage 300 300 [outline t |> filled black]
--}
+--}
